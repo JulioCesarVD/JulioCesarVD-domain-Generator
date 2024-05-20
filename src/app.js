3acf9e77b50eefad957c32f226e49c22a88e223d
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-05-20 08:01:03 +0000

    add domain functions and functions for env

--- src/app.js
+++ src/app.js
@@ -1,9 +1,45 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "dotenv/config";
+require("dotenv").config();
+console.log(process.env);
+
+// Función para generar dominios
+function generateDomains() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+  let hacks = { es: "pued", it: "cred", in: "log" }; // Añade más "hacks" aquí
+
+  let resultsDiv = document.getElementById("domainResults");
+  resultsDiv.innerHTML = ""; // Limpiar resultados anteriores
+
+  // Generar dominios normales
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domain = `${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`;
+          resultsDiv.innerHTML += `<p>${domain}</p>`;
+        }
+      }
+    }
+  }
+
+  // Generar "domain hacks"
+  for (let ext in hacks) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        let domain = `${pronoun[i]}${adj[j]}${hacks[ext]}.${ext}`;
+        resultsDiv.innerHTML += `<p>${domain}</p>`;
+      }
+    }
+  }
+}
 
 window.onload = function() {
   //write your code here
@@ -22,8 +58,8 @@ window.onload = function() {
       let availabilityMessage = document.getElementById("availabilityMessage");
 
       // Tu clave de API de whoisxmlapi.com
-      const apiKey = "El mundo necesita libertad";
-
+      const apiKey = process.env.API_KEY_TEST;
+      console.log(`ESTA ES LA API KEY ${apiKey}`);
       // Realizar la solicitud a la API de whoisxmlapi.com
       fetch(
         `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${apiKey}&domainName=${domainName}&outputFormat=JSON`
@@ -45,3 +81,6 @@ window.onload = function() {
         });
     });
 };
+
+// Exportar la función para que esté disponible globalmente
+window.generateDomains = generateDomains;
